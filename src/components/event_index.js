183445096b12d5e90670ec7ae0ea109b3eabcb11
githubdev58yashi <dev58yashi@gmail.com>
2021-03-22 23:15:33 +0900

    aply material-ui to eventIndex

--- src/components/event_index.js
+++ src/components/event_index.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { Link } from 'react-router-dom';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 import { readEvents } from '../actions';
 
@@ -33,22 +43,48 @@ class EventsIndex extends Component {
 
   renderEvents() {
     return _.map(this.props.events, event => (
-      <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>
+      <TableRow key={event.id}>
+        <TableRowColumn>{event.id}</TableRowColumn>
+        <TableRowColumn>
           <Link to={`/events/${event.id}`}>
             {event.title}
           </Link>
-        </td>
-        <td>{event.body}</td>
-      </tr>
+        </TableRowColumn>
+        <TableRowColumn>{event.body}</TableRowColumn>
+      </TableRow>
     ));
   }
 
   render() {
+    const style = {
+      position: "fixed",
+      right: 12,
+      bottom: 12
+    }
     return (
       <React.Fragment>
-        <table>
+        <FloatingActionButton style={style} containerElement={<Link to="/event/new"></Link>}>
+          <ContentAdd />
+        </FloatingActionButton>
+        <Table>
+          <TableHeader
+            displaySelectAll={false}
+            adjustForCheckbox={false}
+          >
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Title</TableHeaderColumn>
+              <TableHeaderColumn>Body</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody
+            displayRowCheckbox={false}
+          >
+            {this.renderEvents()}
+          </TableBody>
+        </Table>
+        {/* <table>
             <thead>
               <tr>
                 <th>ID</th>
@@ -59,9 +95,9 @@ class EventsIndex extends Component {
             <tbody>
               {this.renderEvents()}
             </tbody>
-        </table>
+        </table> */}
 
-        <Link to="/event/new">new Event</Link>
+        
       </React.Fragment>
     );
   }
