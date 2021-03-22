62cd19bd82031fdf2918eeeba3ac1adb615d2a35
githubdev58yashi <dev58yashi@gmail.com>
2021-03-22 23:42:39 +0900

    apply material-ui to events_new and events_show

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import { composeWithDevTools} from 'redux-devtools-extension';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers';
@@ -19,17 +20,19 @@ const enhancer = process.env.NODE_ENV === 'development' ?
 const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/:id" component={EventsShow} ></Route>
-        <Route path="/event/new" component={EventsNew} ></Route>
-        <Route path="/" component={EventsIndex} ></Route>
-        <Route path="/events" component={EventsIndex} ></Route>
-      </Switch>
-    </BrowserRouter>
-    {/* <EventsIndex /> */}
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/:id" component={EventsShow} ></Route>
+          <Route path="/event/new" component={EventsNew} ></Route>
+          <Route path="/" component={EventsIndex} ></Route>
+          <Route path="/events" component={EventsIndex} ></Route>
+        </Switch>
+      </BrowserRouter>
+      {/* <EventsIndex /> */}
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
